a341c4b334a694963dbf8a68faa53bb19e5b249c
Hunter-Zandri <zandrihunter@gmail.com>
2022-02-23 21:49:32 -0500

    started to upload projects onto project page

--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -5,7 +5,7 @@ function Resume() {
     <section className="my-5">
       <div className="my-2">
       <p>
-        Download my <a href="https://www.linkedin.com/">resume</a>
+        Download my <a href="https://docs.google.com/document/d/1JE7F5WZAHSeKaTfFx5K1SVQDfVlAjhCckibDgLpk9U0/edit?usp=sharing">resume</a>
         </p>
         <h3>Front-end Proficiencies</h3>
         <ul className="skills">
