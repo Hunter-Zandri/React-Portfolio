78d4e15416b7015a76db5af31214daffcc72697d
Hunter-Zandri <zandrihunter@gmail.com>
2022-03-04 22:57:45 -0500

    updated bio and picture link

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import photo from '/Users/hunterzandri/Desktop/Bootcamp/React-Portolio/src/assets/cover/IMG_6190.jpeg'
+import photo from '/Users/hunterzandri/Desktop/Bootcamp/React-Portfolio/src/assets/cover/IMG_6190.jpg'
 
 function About() {
   return (
@@ -10,8 +10,8 @@ function About() {
         {/* <i className="fas fa-user-circle" style={{ fontSize: "96px" }}></i> */}
         </div>
         <p>
-          My Name is HUnter Zandri, I'm Kennebunk Maine and im currently enrolled in UNH full stack coding bootcamp. 
-          I work at an engineeering startup were we build gas delivery systems for the semiconductor Industry. 
+          My Name is Hunter Zandri, I'm Kennebunk Maine and im currently enrolled in UNH full stack coding bootcamp. 
+          I work at an engineering startup were we build gas delivery systems for the semiconductor Industry. 
           I want to use what I what i learn in class in work and continue learning and practicing daily.
 
 </p>
